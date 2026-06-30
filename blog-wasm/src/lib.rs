89d45df480f7ac9f61e52fd954615e936a8061cc
vasiliy1305 <basil.anenkov@yandex.ru>
2026-06-30 18:27:32 +0300

    first build

--- blog-wasm/src/lib.rs
+++ blog-wasm/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn greet(name: &str) -> String {
+    format!("Привет, {name}! Это blog-wasm.")
 }
