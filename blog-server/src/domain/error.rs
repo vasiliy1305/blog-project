77129493c36e4f9a5b3119393cf5402a5e0e46d1
vasiliy1305 <basil.anenkov@yandex.ru>
2026-07-01 14:28:32 +0300

    create domain

--- blog-server/src/domain/error.rs
+++ blog-server/src/domain/error.rs
@@ -2,6 +2,24 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum DomainError {
+    #[error("User {0} not found")]
+    UserNotFound(u64),
+
+    #[error("User {0} already exists")]
+    UserAlreadyExists(u64),
+
+    #[error("Post {0} not found")]
+    PostNotFound(u64),
+
+    #[error("Invalid credentials")]
+    InvalidCredentials,
+
+    #[error("Access forbidden")]
+    Forbidden,
+
     #[error("Password hash error: {0}")]
     PasswordHash(String),
+
+    #[error("Validation failed: {0}")]
+    Validation(String),
 }
